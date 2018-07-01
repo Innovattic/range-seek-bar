45afcc986f8f825b2ddc591f9cba5824105d3a30
Mohammad Mirrajabi <mohammadmirrajabi@gmail.com>
2018-07-01 13:25:31 +0200

    Changed `app` module name to `sample`

--- sample/src/main/java/com/innovattic/rangeseekbar/sample/MainActivity.kt
+++ sample/src/main/java/com/innovattic/rangeseekbar/sample/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.innovattic.rangeseekbar.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
